568c05fb0a1dbbf59fe40e41bdda8c3f6994f00a
Goh Chong Yong <gcyong2004@gmail.com>
2024-01-04 17:52:10 +0800

    updates

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/.well-known/apple-app-site-association',
+                headers: [
+                    {
+                        key: 'Content-Type',
+                        value: 'application/json'
+                    }
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
